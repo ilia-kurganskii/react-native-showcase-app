9f14959647a2291b3749d3725f7f588254d8a0be
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-06-06 20:36:02 +0200

    chore(deps): add react-native navigation add husky and pre-commit linting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  "quoteProps": "consistent",
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false,
 };
