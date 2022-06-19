5d1e3b1c42eae8123b1487c60323efa1302eebeb
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-06-19 11:40:47 +0200

    feat(login): implement login and signup screens

--- src/features/login/screens/sign-up/sign-up.screen.tsx
+++ src/features/login/screens/sign-up/sign-up.screen.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Button } from 'react-native-nucleus-ui';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { KeyboardAvoidingView, ScrollView, Text } from 'react-native';
+import { Button, TextField } from 'react-native-nucleus-ui';
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 import { useSignUpController } from './sign-up.controller';
 
-export function SignUpScreen() {
-  const { signUp } = useSignUpController();
+function SignUpScreenComponent() {
+  const { signUp, styles } = useSignUpController();
+  const { top } = useSafeAreaInsets();
   return (
-    <SafeAreaView testID="sign-up-screen">
-      <Text>Create account</Text>
-      <Button onPress={signUp} title="Create account" />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={top + 10}
+      style={styles.keyboardView}
+    >
+      <ScrollView
+        testID="sign-up-screen"
+        contentContainerStyle={styles.scrollContainer}
+      >
+        <SafeAreaView edges={['bottom']} style={styles.safeContainer}>
+          <Text style={styles.label}>Email</Text>
+          <TextField
+            autoCapitalize="none"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Password</Text>
+          <TextField secureTextEntry={true} style={styles.input} />
+
+          <Button
+            title="Create account"
+            style={styles.button}
+            onPress={signUp}
+            size="large"
+          />
+        </SafeAreaView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
+
+export const SignUpScreen = React.memo(SignUpScreenComponent);
