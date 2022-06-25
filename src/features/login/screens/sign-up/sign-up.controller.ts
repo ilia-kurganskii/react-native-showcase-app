f0c2c4e910f75e99af57ff796bf24c572c418e31
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-06-25 11:56:41 +0200

    feat(signup): use returnKey option to improve UI

--- src/features/login/screens/sign-up/sign-up.controller.ts
+++ src/features/login/screens/sign-up/sign-up.controller.ts
@@ -3,8 +3,12 @@ import { useTheme } from 'react-native-nucleus-ui';
 import { getSignUpScreenStyles } from './sign-up.style';
 import { extendThemeWithSignUp } from './sign-up.theme';
 import { useSignUpForm } from './sign-up.form';
+import { useCallback, useRef } from 'react';
+import { TextInput } from 'react-native';
 
 export function useSignUpController() {
+  const passwordRef = useRef<TextInput>(null);
+
   const authStore = useAuthStore();
   const theme = useTheme();
   const form = useSignUpForm(({ email, password }) =>
@@ -12,10 +16,16 @@ export function useSignUpController() {
   );
   const styles = getSignUpScreenStyles(extendThemeWithSignUp(theme));
 
+  const focusOnPassword = useCallback(() => {
+    passwordRef.current?.focus();
+  }, [passwordRef]);
+
   return {
     signUp: form.submitForm,
     styles,
     emailField: form.emailField,
     passwordField: form.passwordField,
+    passwordRef,
+    focusOnPassword,
   };
 }
