d37a422620363ba5c92a930e06baddb621d5d48e
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-08-29 22:34:37 +0200

    build: update renovate config (#157)

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+  allowedPostUpgradeCommands: ['*'],
+  repositories: ['ilia-kurganskii/react-native-showcase-app'],
+};
