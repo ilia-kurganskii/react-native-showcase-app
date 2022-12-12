a72c45da4b4064265f0289a6979099ca3fbeddda
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-12-12 21:08:46 +0100

    feat: implement translation service

--- i18next-scanner.config.js
+++ i18next-scanner.config.js
@@ -12,6 +12,24 @@ module.exports = {
       extensions: ['.ts', '.tsx'],
     },
     lngs: ['en', 'ru'],
+    trans: {
+      component: 'Trans',
+      i18nKey: 'i18nKey',
+      defaultsKey: 'defaults',
+      extensions: ['.tsx'],
+      fallbackKey: function (ns, value) {
+        return value;
+      },
+
+      // https://react.i18next.com/latest/trans-component#usage-with-simple-html-elements-like-less-than-br-greater-than-and-others-v10.4.0
+      supportBasicHtmlNodes: false, // Enables keeping the name of simple nodes (e.g. <br/>) in translations instead of indexed keys.
+
+      // https://github.com/acornjs/acorn/tree/master/acorn#interface
+      acorn: {
+        ecmaVersion: 2020,
+        sourceType: 'module', // defaults to 'module'
+      },
+    },
     defaultLng: 'en',
     defaultValue: '__STRING_NOT_TRANSLATED__',
     resource: {
