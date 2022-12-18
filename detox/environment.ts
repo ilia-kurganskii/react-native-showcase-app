2be53c0ea117d147d27fa0d22b5df0365485af60
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-12-18 17:52:02 +0100

    chore: fix jest config

--- detox/environment.ts
+++
@@ -1,7 +0,0 @@
-const { DetoxCircusEnvironment } = require('detox/runners/jest');
-
-export default class CustomDetoxEnvironment extends DetoxCircusEnvironment {
-  constructor(config: any, context: any) {
-    super(config, context);
-  }
-}
