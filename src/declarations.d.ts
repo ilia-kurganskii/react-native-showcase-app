482d495ac16d910acc0d3ff1b716f6a25fd4e0a4
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-12-26 13:19:21 +0100

    chore: add types for i18n

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,6 +1,18 @@
+import 'i18next';
+import ns1 from './features/i18n/resources/en/translation.json';
+
 declare module '*.svg' {
   import React from 'react';
   import { SvgProps } from 'react-native-svg';
   const content: React.FC<SvgProps>;
   export default content;
+}
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'ns1';
+    resources: {
+      ns1: typeof ns1;
+    };
+  }
 }
